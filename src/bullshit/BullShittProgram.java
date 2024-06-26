75010b8a544b26a01ef8e488a8094bd6a81297bd
SrishamSampekatte <srishamsm.xworkz@gmail.com>
2024-06-26 23:36:44 +0530

    first commit

---
+++ src/bullshit/BullShittProgram.java
@@ -0,0 +1,11 @@
+package bullshit;
+
+public class BullShittProgram {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("This is bull shit program..!   ");
+
+	}
+
+}
